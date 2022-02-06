87a04fcd3c7c51d1ecdd7a19474d369ce9063fd3
Raul Rosenlöcher <git@home>
2022-02-06 13:50:30 +0100

    config: added "yaml_templates" section for "referenced" objects

--- worker_bunch/service_config.py
+++ worker_bunch/service_config.py
@@ -9,21 +9,33 @@ class ConfigException(Exception):
 
 
 class MainConfKey:
+    DATABASE_CONNECTIONS = "database_connections"
     LOGGING = "logging"
     MQTT_BROKER = "mqtt_broker"
-    DATABASE_CONNECTIONS = "database_connections"
     WORKER_INSTANCES = "worker_instances"
     WORKER_SETTINGS = "worker_settings"
+    YAML_TEMPLATES = "yaml_templates"
 
 
 CONFIG_JSONSCHEMA = {
     "type": "object",
     "properties": {
+        MainConfKey.DATABASE_CONNECTIONS: DATABASE_CONNECTIONS_JSONSCHEMA,
         MainConfKey.LOGGING: LOGGING_JSONSCHEMA,
         MainConfKey.MQTT_BROKER: MQTT_JSONSCHEMA,
         MainConfKey.WORKER_INSTANCES: WORKER_INSTANCES_JSONSCHEMA,
-        MainConfKey.WORKER_SETTINGS: {"type": "object"},  # dummy
-        MainConfKey.DATABASE_CONNECTIONS: DATABASE_CONNECTIONS_JSONSCHEMA,
+        MainConfKey.WORKER_SETTINGS: {
+            "type": "object",
+            "description": "dummy section, will be filled with worker schemes based on configuration."
+        },
+        MainConfKey.YAML_TEMPLATES: {
+            "type": "object",
+            "additionalProperties": True,
+            "description": "The strict schema prevents putting objects anywhere outside of given structure. This constrains the YAML "
+                           "reference feature. Therefor this section is supposed to contain arbitrary objects without a schema validation. "
+                           "Also nothing within this section is used by worker-bunch directly. So put your reused YAML referenced objects "
+                           "here."
+        },
     },
     "additionalProperties": False,
     "required": [MainConfKey.WORKER_INSTANCES],
