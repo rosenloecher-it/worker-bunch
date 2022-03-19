01eedd99e8634d634ac4e477ac4dd71a994d15cc
Raul Rosenlöcher <git@home>
2022-03-19 19:01:09 +0100

    AstralTimesPublisher

---
+++ test/astral_times/test_astral_times_publisher.py
@@ -0,0 +1,57 @@
+import unittest
+from datetime import datetime, timedelta, timezone
+from unittest import mock
+
+from worker_bunch.astral_times.astral_times_config import AstralTimesConfKey
+from worker_bunch.astral_times.astral_times_manager import AstralTimesManager
+from worker_bunch.astral_times.astral_times_publisher import AstralTimesPublisher, AstralTimesPublisherConfKey
+from worker_bunch.dispatcher import Dispatcher
+from worker_bunch.mqtt.mqtt_proxy import MqttProxy
+from worker_bunch.notification import Notification
+
+
+class TestAstralTimesPublisher(unittest.TestCase):
+
+    DUMMY_CONFIG = {
+        AstralTimesConfKey.LATITUDE: 51.051873,
+        AstralTimesConfKey.LONGITUDE: 13.741522,
+        AstralTimesConfKey.ELEVATION: 125,
+    }
+
+    SETTINGS = {
+        AstralTimesPublisherConfKey.MQTT_TOPIC_OUT: AstralTimesPublisherConfKey.MQTT_TOPIC_OUT,
+        AstralTimesPublisherConfKey.MQTT_LAST_WILL: AstralTimesPublisherConfKey.MQTT_LAST_WILL,
+        AstralTimesPublisherConfKey.MQTT_RETAIN: True,
+    }
+
+    def setUp(self):
+        self.manager = AstralTimesManager(self.DUMMY_CONFIG)
+        self.dispatcher = Dispatcher(self.manager)
+        self.mqtt_proxy = None  # MagicMock[MqttProxy]
+
+        self.worker = AstralTimesPublisher("test")
+        self.worker.set_extra_settings(self.SETTINGS)
+
+        self.reset_mqtt_proxy()
+
+    def reset_mqtt_proxy(self):
+        self.mqtt_proxy = mock.MagicMock(MqttProxy, autospec=True)
+        self.worker.set_mqtt_proxy(self.mqtt_proxy)
+
+    @mock.patch("worker_bunch.time_utils.TimeUtils.now")
+    def test_round_trip(self, mocked_now):
+        now = datetime(2022, 3, 19, 9, 55, 15, tzinfo=timezone(timedelta(seconds=3600)))
+        mocked_now.return_value = now
+
+        self.worker.subscribe_notifications(self.dispatcher)
+
+        self.worker._work([Notification.create_cron("does-it-matter?")])
+
+        expected_payload = self.manager.get_astral_times(now)
+        expected_payload["status"] = "ok"
+
+        self.mqtt_proxy.queue.assert_called_once_with(
+            topic=AstralTimesPublisherConfKey.MQTT_TOPIC_OUT,
+            payload=expected_payload,
+            retain=True
+        )
---
+++ worker_bunch/astral_times/astral_times_publisher.py
@@ -0,0 +1,77 @@
+from typing import Dict, Optional, List
+
+from worker_bunch.astral_times.astral_times_manager import AstralTimesManager
+from worker_bunch.dispatcher import Dispatcher
+from worker_bunch.notification import Notification, NotificationType
+from worker_bunch.worker.worker import Worker
+
+
+class AstralTimesPublisherConfKey:
+    MQTT_TOPIC_OUT = "mqtt_topic_out"
+    MQTT_LAST_WILL = "mqtt_last_will"
+    MQTT_RETAIN = "mqtt_retain"
+
+
+ASTRAL_TIMES_PUBLISHER_JSONSCHEMA = {
+    "type": "object",
+    "additionalProperties": False,
+    "properties": {
+        AstralTimesPublisherConfKey.MQTT_LAST_WILL: {"type": "string", "minLength": 1, "description": "MQTT last will message"},
+        AstralTimesPublisherConfKey.MQTT_RETAIN: {"type": "boolean", "description": "Make MQTT message persistent."},
+        AstralTimesPublisherConfKey.MQTT_TOPIC_OUT: {"type": "string", "minLength": 1, "description": "MQTT topic to write to"},
+    },
+    "required": [AstralTimesPublisherConfKey.MQTT_TOPIC_OUT]
+}
+
+
+class AstralTimesPublisher(Worker):
+
+    def __init__(self, name: str):
+        super().__init__(name)
+
+        self._mqtt_last_will = ""
+        self._mqtt_retain = False
+        self._mqtt_topic_out = []
+        self._mqtt_topics_in = ""
+
+        self._astral_time_manager: Optional[AstralTimesManager] = None
+
+    def set_extra_settings(self, extra_settings: Optional[Dict[str, any]]):
+        super().set_extra_settings(extra_settings)
+
+        self._mqtt_last_will = self._extra_settings.get(AstralTimesPublisherConfKey.MQTT_LAST_WILL)
+        self._mqtt_retain = self._extra_settings.get(AstralTimesPublisherConfKey.MQTT_RETAIN, False)
+        self._mqtt_topic_out = self._extra_settings[AstralTimesPublisherConfKey.MQTT_TOPIC_OUT]
+
+    def get_partial_settings_schema(self) -> Dict[str, any]:
+        return ASTRAL_TIMES_PUBLISHER_JSONSCHEMA
+
+    def make_partial_settings_required(self) -> bool:
+        return True
+
+    def set_last_will(self):
+        """Set the last will at mqtt client. Explicitly triggered before the mqtt client connects."""
+        if self._mqtt_topic_out and self._mqtt_last_will:
+            self._mqtt_proxy.set_last_will(self._mqtt_topic_out, self._mqtt_last_will)
+
+    def subscribe_notifications(self, dispatcher: Dispatcher):
+
+        self._astral_time_manager = dispatcher.get_astral_time_manager()
+
+        dispatcher.subscribe_cron(self, "* 0 * * *", "cron-every-hour")
+
+    def _work(self, notifications: List[Notification]):
+        found_message = next((n for n in notifications if n.type in [NotificationType.CRON, NotificationType.JUST_STARTED]), None)
+
+        if found_message:
+            if self._astral_time_manager:
+                astral_times = self._astral_time_manager.get_astral_times()
+                astral_times["status"] = "ok"
+                self._mqtt_proxy.queue(topic=self._mqtt_topic_out, payload=astral_times, retain=self._mqtt_retain)
+            else:
+                self._final_work()
+
+    def _final_work(self):
+        """sends the last will"""
+        if self._mqtt_topic_out and self._mqtt_last_will:
+            self._mqtt_proxy.queue(self._mqtt_topic_out, self._mqtt_last_will, retain=self._mqtt_retain)
