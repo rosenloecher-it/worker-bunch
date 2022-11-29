36c48b9e13f8daaed6b7dfb8e7e412bcbf1491e0
Raul Rosenlöcher <git@home>
2022-11-29 17:30:11 +0100

    preserve timezone in JSON

--- worker_bunch/utils/json_utils.py
+++ worker_bunch/utils/json_utils.py
@@ -1,13 +1,17 @@
 import datetime
 import json
 
+from worker_bunch.utils.time_utils import TimeUtils
+
 
 class JsonUtils:
 
     @classmethod
     def _default_json_serial(cls, obj):
         """JSON serializer for objects not serializable by default json code"""
-        if isinstance(obj, (datetime.datetime, datetime.date)):
+        if isinstance(obj, datetime.datetime):
+            return TimeUtils.iso_tz(obj)
+        elif isinstance(obj, datetime.date):
             return obj.isoformat()
 
         raise TypeError(f"Type '{type(obj)}' is not JSON serializable!")
